591515253c21dbb243d68a97e113a1377dd85936
pancevac <sinisa.buncic7@gmail.com>
2019-09-13 15:57:49 +0200

    Init modified side menu

--- src/components/AppDrawer.vue
+++ src/components/AppDrawer.vue
@@ -1,13 +1,39 @@
 <template>
-  
+  <v-navigation-drawer
+      app
+      v-model="drawer"
+      clipped
+  >
+    <v-list dense>
+      <v-list-item
+        v-for="(link, key) in links"
+        :key="key"
+        @click="$router.push({name: link.route})"
+      >
+
+        <v-list-item-icon>
+          <v-icon>{{ link.icon }}</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title v-text="link.text"></v-list-item-title>
+
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <script>
   export default {
-    name: "AppDrawer"
+    name: "AppDrawer",
+
+    props: {
+      drawer: Boolean
+    },
+
+    data: () => ({
+      links: [
+        {icon: "mdi-home", text: "Trips", route: "trips"},
+      ]
+    })
+
   }
 </script>
-
-<style scoped>
-
-</style>
